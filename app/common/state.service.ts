4d97e9264493d9733a429473b4fac25d7993936b
greglindahl <greglindahl410@gmail.com>
2016-05-07 15:30:11 -0600

    Initial commit

--- app/common/state.service.ts
+++ app/common/state.service.ts
@@ -1,8 +1,14 @@
 import {Injectable} from 'angular2/core';
+import {Config} from './config.model'
 
 @Injectable()
 export class StateService {
 	private _message = 'Hello Message';
+  superGreeting: string = 'No! YOU HOLLA T YER BOY!';
+  config: Config ={
+    name: 'Hello',
+    description: 'Description'
+  };
 
   getMessage(): string {
     return this._message;
--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from 'angular2/core';
 import {StateService} from '../common/state.service';
+import {Config} from '../common/config.model';
 
 @Component({
   selector: 'home',
@@ -8,12 +9,16 @@ import {StateService} from '../common/state.service';
 export class HomeComponent implements OnInit {
   title: string = 'Home Page';
   body:  string = 'This is the about home body';
+  greeting: string;
   message: string;
+  config: Config;
 
   constructor(private _stateService: StateService) { }
 
   ngOnInit() {
     this.message = this._stateService.getMessage();
+    this.greeting = this._stateService.superGreeting;
+    this.config = this._stateService.config;
   }
 
   updateMessage(m: string): void {
